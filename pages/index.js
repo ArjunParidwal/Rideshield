eff017a3e5a3e379fabb6ff1afb5516040d44c32
Arjun <arjunparidwal1998@gmail.com>
2022-07-08 17:42:19 +0530

    nesw

--- pages/index.js
+++ pages/index.js
@@ -2,31 +2,31 @@
 export default function Home() {
   return (
     <>
-<div class="container-fluid">
-    <div class="row b">
-    <div class="col-md-4 siderbar">
-    <ul class="nav flex-column">
-    <li class="nav-item">
+<div className="container-fluid">
+    <div className="row b">
+    <div className="col-md-4 siderbar">
+    <ul className="nav flex-column">
+    <li className="nav-item">
   <h4>Ride Shield</h4>
   </li>
-  <li class="nav-item">
-  <a href="google.com"><i class="bi bi-house-fill"></i><span class="ms-1 d-none d-sm-inline">Home</span></a>
+  <li className="nav-item">
+  <a href="google.com"><i className="bi bi-house-fill"></i><span className="ms-1 d-none d-sm-inline">Home</span></a>
   </li>
-  <li class="nav-item">
-  <i class="bi bi-battery"></i><span class="ms-1 d-none d-sm-inline"  href="#">Battery Overview</span>
+  <li className="nav-item">
+  <i className="bi bi-battery"></i><span className="ms-1 d-none d-sm-inline"  href="#">Battery Overview</span>
   </li>
-  <li class="nav-item">
-  <i class="bi bi-truck"></i><span class="ms-1 d-none d-sm-inline"  href="#">Fleet Overview</span>
+  <li className="nav-item">
+  <i className="bi bi-truck"></i><span className="ms-1 d-none d-sm-inline"  href="#">Fleet Overview</span>
   </li>
-  <li class="nav-item">
-  <i class="bi bi-battery-charging"></i><span class="ms-1 d-none d-sm-inline"  href="#">Battery Details</span>
+  <li className="nav-item">
+  <i className="bi bi-battery-charging"></i><span className="ms-1 d-none d-sm-inline"  href="#">Battery Details</span>
   </li>
-  <li class="nav-item">
-  <i class="bi bi-pie-chart-fill"></i><span class="ms-1 d-none d-sm-inline"  href="#">Reports</span>
+  <li className="nav-item">
+  <i className="bi bi-pie-chart-fill"></i><span className="ms-1 d-none d-sm-inline"  href="#">Reports</span>
   </li>
 </ul>
     </div>
-    <div class="col-md-6 rightbar">
+    <div className="col-md-6 rightbar">
       2
     </div>
     </div>
