9af8965cdec89993a97bc697dc5037f792e51e0d
Arjun <arjunparidwal1998@gmail.com>
2022-07-14 17:15:39 +0530

    scripts

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ import Script from 'next/script';
 export default function Home() {
   return (
     <div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"/>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" async ></script>
 <div className="container-fluid">
     <div className="row b">
       <Menu />
