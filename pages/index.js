9593352e624f34fefb528c49d6f34145899810f9
Arjun <arjunparidwal1998@gmail.com>
2022-07-14 16:18:53 +0530

    mobilemenu

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,14 @@
 import Sidebar from "../components/sidebar"
 import Dashboard from "../components/dashboard"
 import Menu from "../components/menu"
+import Script from 'next/script'
+
 
 
 export default function Home() {
   return (
     <>
-    <head>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
-    </head>
 <div className="container-fluid">
     <div className="row b">
       <Menu />
