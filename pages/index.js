b97d9233deae29c041e625b6e3ab52e30de957dd
Arjun <arjunparidwal1998@gmail.com>
2022-07-14 16:22:10 +0530

    mobilemenu

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ import Script from 'next/script'
 export default function Home() {
   return (
     <>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous" async />
 <div className="container-fluid">
     <div className="row b">
       <Menu />
