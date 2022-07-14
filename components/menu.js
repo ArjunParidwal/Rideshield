2807c88ebdb15dcc7381b57b13f87b63bd101d2a
Arjun <arjunparidwal1998@gmail.com>
2022-07-14 16:10:57 +0530

    mobilemenu

--- components/menu.js
+++ components/menu.js
@@ -5,43 +5,43 @@ export default class menu extends Component {
   render() {
     return (
 <>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-   <div class="container-fluid">
-      <a class="navbar-brand" href="/">Ride Shield</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+<nav className="navbar navbar-expand-lg navbar-light bg-light">
+   <div className="container-fluid">
+      <a className="navbar-brand" href="/">Ride Shield</a>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarText">
-         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
+      <div className="collapse navbar-collapse" id="navbarText">
+         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
                <i className="bi bi-house-fill"></i>
                <span className="ms-1  d-sm-inline">
                   <Link href="/">
                   <span className='al'>Home</span></Link>
                </span>
             </li>
-            <li class="nav-item">
+            <li className="nav-item">
                <i className="bi bi-battery"></i>
                <span className="ms-1  d-sm-inline" >
                   <Link href="Battery_Overview">
                   <span className='al'>Battery Overview</span></Link>
                </span>
             </li>
-            <li class="nav-item">
+            <li className="nav-item">
                <i className="bi bi-truck"></i>
                <span className="ms-1  d-sm-inline">
                   <Link href="Fleet_Overview">
                   <span className='al'>Fleet Overview</span></Link>
                </span>
             </li>
-            <li class="nav-item">
+            <li className="nav-item">
                <i className="bi bi-battery-charging"></i>
                <span className="ms-1  d-sm-inline">
                   <Link href="/">
                   <span className='al'>Battery Details</span></Link>
                </span>
             </li>
-            <li class="nav-item">
+            <li className="nav-item">
                <i className="bi bi-pie-chart-fill"></i>
                <span className="ms-1  d-sm-inline">
                   <Link href="/">
