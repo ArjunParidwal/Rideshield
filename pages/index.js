e70de469c18a0a8c3a2b31985ae9b64fc1073bb9
Arjun <arjunparidwal1998@gmail.com>
2022-07-14 16:16:15 +0530

    mobilemenu

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,10 @@ import Menu from "../components/menu"
 export default function Home() {
   return (
     <>
+    <head>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    </head>
 <div className="container-fluid">
     <div className="row b">
       <Menu />
