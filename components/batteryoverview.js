3f5a0f2417b556bbdb89e8ee84cbc1aaa0a146d2
Arjun <arjunparidwal1998@gmail.com>
2022-07-20 18:08:08 +0530

    page

--- components/batteryoverview.js
+++ components/batteryoverview.js
@@ -6,9 +6,9 @@ import Charts from '../components/charts'
 const batteryoverview = () => {
     return (
         <>
-       <div className="col-md-8 rightbar">
-   <div className="col-md-6 c">
-      <div className="container">
+       <div className="col-md-8 rightbars">
+   <div className="col-md-12 bo">
+      
          <div className="row chart">
             <div className="col-sm-6 chart">
                <Charts />
@@ -95,11 +95,12 @@ const batteryoverview = () => {
                </div>
             </div>
          </div>
-      </div>
+      
    </div>
-   <div className="col-md-3 c">
-      <div className="col-sm-12">
-         <div className="col-sm-12 tableb">
+   <div className="col-md-12 c">
+  <div className="row align-items-center">
+    <div className="col-sm-3">
+    <div className="col-sm-12 tableb">
             <div className='meterbox'>
                <ReactSpeedometer
                width={200}
@@ -114,7 +115,9 @@ const batteryoverview = () => {
                <p className='speedh'>Driving Behavior</p>
             </div>
          </div>
-         <div className="col-sm-12 tableb">
+    </div>
+    <div className="col-sm-3">
+    <div className="col-sm-12 tableb">
             <div className='meterbox'>
                <ReactSpeedometer
                width={200}
@@ -129,7 +132,9 @@ const batteryoverview = () => {
                <p className='speedh'>Average Speed</p>
             </div>
          </div>
-         <div className="col-sm-12 tableb">
+    </div>
+    <div className="col-sm-3">
+    <div className="col-sm-12 tableb">
             <div className='meterbox'>
                <ReactSpeedometer
                width={200}
@@ -144,7 +149,9 @@ const batteryoverview = () => {
                <p className='speedh'>Average Discharge (DOD)</p>
             </div>
          </div>
-         <div className="col-sm-12 tableb">
+    </div>
+    <div className="col-sm-3">
+    <div className="col-sm-12 tableb">
             <div className='meterbox'>
                <ReactSpeedometer
                width={200}
@@ -159,7 +166,9 @@ const batteryoverview = () => {
                <p className='speedh'>Average Charge</p>
             </div>
          </div>
-         <div className="col-sm-12 tableb">
+    </div>
+    <div className="col-sm-3">
+    <div className="col-sm-12 tableb">
             <div className='meterbox'>
                <ReactSpeedometer
                width={200}
@@ -174,10 +183,13 @@ const batteryoverview = () => {
                <p className='speedh'>Mileage</p>
             </div>
          </div>
-      </div>
+    </div>
+  </div>
+  </div>
+ 
+   <div className="col-md-12 map"><iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d14025.513062112626!2d77.22901879999999!3d28.49826495!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1657300133587!5m2!1sen!2sin" width="100%" height="100%" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></div>
+
    </div>
-   <div className="col-md-3 map"><iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d14025.513062112626!2d77.22901879999999!3d28.49826495!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1657300133587!5m2!1sen!2sin" width="100%" height="100%" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></div>
-</div>
         </>
         )
     }
